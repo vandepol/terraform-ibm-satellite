64fbb8d276e1e97db3e584204c76a9ee8fbb7a6a
David VandePol <vandepol@ca.ibm.com>
2022-02-23 11:31:37 -0500

    hack

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
-  # subscription_id = var.subscription_id
-  # client_id       = var.client_id
-  # tenant_id       = var.tenant_id
-  # client_secret   = var.client_secret
+   subscription_id = var.ARM_SUBSCRIPTION_ID
+   client_id       = var.ARM_CLIENT_ID
+   tenant_id       = var.ARM_TENANT_ID
+   client_secret   = var.ARM_CLIENT_SECRET
 }
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
