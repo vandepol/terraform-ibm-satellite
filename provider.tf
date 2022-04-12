199e6e289ff615fd4c674546c2d49f3af6c91e27
David VandePol <vandepol@ca.ibm.com>
2022-04-12 17:17:10 -0400

    remove variables

--- provider.tf
+++ provider.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   features {}
-   subscription_id = var.ARM_SUBSCRIPTION_ID
-   client_id       = var.ARM_CLIENT_ID
-   tenant_id       = var.ARM_TENANT_ID
-   client_secret   = var.ARM_CLIENT_SECRET
 }
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
