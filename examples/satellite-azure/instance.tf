ae0c2b9f7aa78fead335a0535b6d7259f860408a
David VandePol <vandepol@ca.ibm.com>
2022-04-13 11:20:17 -0400

    tcp ---> Tcp

--- examples/satellite-azure/instance.tf
+++ examples/satellite-azure/instance.tf
@@ -40,7 +40,7 @@ module "network-security-group" {
       priority                   = 110
       direction                  = "Inbound"
       access                     = "Allow"
-      protocol                   = "tcp"
+      protocol                   = "Tcp"
       source_port_range          = "*"
       destination_port_range     = "22"
       source_address_prefix      = "*"
